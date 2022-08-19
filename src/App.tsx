7caccd5188ea413e128d5a51f03582d57abe018d
Oleg Tkach <KratosHome101@gmail.com>
2022-08-19 13:15:28 +0300

    create project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.scss';
+import catsImg from "./img/cats.jpg"
+import chubakaImg from "./img/chubaka.jpg"
+import dogsImg from "./img/dogs.jpg"
+import dynoImg from "./img/dyno.jpg"
+import planetsImg from "./img/planets.jpg"
+
+
+const data = [
+    {
+        img: catsImg
+    },
+    {
+        img: chubakaImg
+    },
+    {
+        img: dogsImg
+    },
+    {
+        img: dynoImg
+    },
+    {
+        img: planetsImg
+    }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [header, setheader] = useState(data[Math.floor(Math.random() * data.length)])
+    const [left, setleft] = useState(data[Math.floor(Math.random() * data.length)])
+    const [right, setright] = useState(data[Math.floor(Math.random() * data.length)])
+    const [button, setbutton] = useState(data[Math.floor(Math.random() * data.length)])
+    const [shangeColorButton, setShangeColorButton] = useState(false)
+
+
+    const clockButton = () => {
+        setheader(data[Math.floor(Math.random() * data.length)])
+        setleft(data[Math.floor(Math.random() * data.length)])
+        setright(data[Math.floor(Math.random() * data.length)])
+        setbutton(data[Math.floor(Math.random() * data.length)])
+        setShangeColorButton(!shangeColorButton)
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            clockButton()
+        }, 52000);
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <div className=" App">
+            <header>
+                <img src={header.img}/>
+            </header>
+            <div className="containerMain">
+                <div className="leftBlock">
+                    <img src={left.img}/>
+                </div>
+                <button
+                    onClick={() => clockButton()}
+                    style={{
+                        backgroundColor: shangeColorButton ? "red" : "green"
+                    }}>
+                    click
+                </button>
+                <div className="rightBlock">
+                    <img src={right.img}/>
+                </div>
+            </div>
+            <footer>
+                <img src={button.img}/>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
